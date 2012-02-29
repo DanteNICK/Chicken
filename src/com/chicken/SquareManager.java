99b93fc57b79f2c8c0477d57d88e7a3c34f271ed
Igor Mats <igor.mats@yandex.ru>
2012-02-29 20:56:48 +0200

    Add SquareManager

---
+++ src/com/chicken/SquareManager.java
@@ -0,0 +1,18 @@
+package com.chicken;
+
+public class SquareManager {
+
+	public Element[][] matrix;
+
+	public SquareManager() {
+		matrix = new Element[7][7];
+	}
+
+	public void create() {
+	}
+
+	public void create() {
+
+	}
+
+}
