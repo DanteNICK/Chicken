85fc63bf87de3ce55994cc2712cabf7c10b4402c
Olivia1411 <appossumik@gmail.com>
2012-02-29 20:41:40 +0200

    Added BaseGameActivity

--- src/com/chicken/GameActivity.java
+++ src/com/chicken/GameActivity.java
@@ -1,13 +1,34 @@
 package com.chicken;
 
-import android.app.Activity;
-import android.os.Bundle;
+import org.anddev.andengine.engine.Engine;
+import org.anddev.andengine.entity.scene.Scene;
+import org.anddev.andengine.ui.activity.BaseGameActivity;
 
-public class GameActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+
+public class GameActivity extends BaseGameActivity {
+
+	@Override
+	public void onLoadComplete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Engine onLoadEngine() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void onLoadResources() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Scene onLoadScene() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+    
 }
