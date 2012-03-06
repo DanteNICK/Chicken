8d13e7a49d83a316be91cb091a9386ad8db1145b
Olivia1411 <appossumik@gmail.com>
2012-03-06 22:46:52 +0200

    created check of moving

--- src/com/chicken/SquareManager.java
+++ src/com/chicken/SquareManager.java
@@ -1,5 +1,6 @@
 package com.chicken;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.anddev.andengine.opengl.texture.atlas.bitmap.BitmapTextureAtlasTextureRegionFactory;
@@ -62,25 +63,85 @@ public class SquareManager {
 				matrix[i][j].update();
 			}
 		}
-		if(el_1 !=null && el_2!=null)
-		{
-			
-			
-		/*Element element = el_1;
-		el_1.setPosition(el_2.getX(), el_2.getY());
-		el_2.setPosition(element.getX(), element.getY());
-		
-		el_1.collNumber = el_2.collNumber;
-		el_2.collNumber = element.collNumber;
-		
-		el_1.rowNumber = el_2.rowNumber;
-		el_2.rowNumber = element.rowNumber;*/
-		
-			
+		if (el_1 != null && el_2 != null) {
+
+			/*
+			 * Element element = el_1; el_1.setPosition(el_2.getX(),
+			 * el_2.getY()); el_2.setPosition(element.getX(), element.getY());
+			 * 
+			 * el_1.collNumber = el_2.collNumber; el_2.collNumber =
+			 * element.collNumber;
+			 * 
+			 * el_1.rowNumber = el_2.rowNumber; el_2.rowNumber =
+			 * element.rowNumber;
+			 */
+
 			el_1 = null;
 			el_2 = null;
-			
+
 		}
 	}
 
+	public boolean movingRezult() {
+		Element a = el_1;
+		Element b = el_2;
+		el_1.rowNumber = b.rowNumber;
+		el_1.collNumber = b.collNumber;
+		el_2.rowNumber = a.rowNumber;
+		el_2.collNumber = a.collNumber;
+
+		boolean movingRezult = false;
+		if (coincidence())
+			movingRezult = true;
+		else {
+			movingRezult = false;
+			el_1.rowNumber = a.rowNumber;
+			el_1.collNumber = a.collNumber;
+			el_2.rowNumber = b.rowNumber;
+			el_2.collNumber = b.collNumber;
+		}
+		return movingRezult;
+	}
+
+	public boolean coincidence() {
+		boolean coincidenceExist = false;
+		ArrayList<Element> currentElements = new ArrayList<Element>();
+		currentElements.add(matrix[0][0]);
+		// search across rows
+		for (int row = 0; row < Options.COUNT_OF_ROW; row++)
+			for (int col = 0; col < Options.COUNT_OF_COLL; col++) {
+				if (currentElements.contains(matrix[row][col]))
+					currentElements.add(matrix[row][col]);
+				else {
+					if (currentElements.size() >= 3) {
+						for (int i = currentElements.size(); i > 0; i--) {
+							matrix[row][col - i] = null;
+							coincidenceExist = true;
+						}
+					} else {
+						currentElements.clear();
+						currentElements.add(matrix[row][col]);
+					}
+				}
+			}
+		// search by columns
+		for (int col = 0; col < Options.COUNT_OF_COLL; col++)
+			for (int row = 0; row < Options.COUNT_OF_ROW; row++) {
+				if (currentElements.contains(matrix[row][col]))
+					currentElements.add(matrix[row][col]);
+				else {
+					if (currentElements.size() >= 3) {
+						for (int i = currentElements.size(); i > 0; i--) {
+							matrix[row - i][col] = null;
+							coincidenceExist = true;
+						}
+					} else {
+						currentElements.clear();
+						currentElements.add(matrix[row][col]);
+					}
+				}
+
+			}
+		return coincidenceExist;
+	}
 }
